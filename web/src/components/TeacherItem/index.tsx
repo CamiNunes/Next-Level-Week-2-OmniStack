6b2f942495ad6acbcab4eadbca54c12bcb93cd28
CamiNunes <cvn.camila@gmail.com>
2020-08-14 14:37:12 -0300

    Finalização da comunicação do frontend com o backend;

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,33 +1,49 @@
 import React from 'react';
-
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './style.css';
 
-function TeacherItem(){
+export interface Teacher{
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps{
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,  
+    })
+  }
+
   return(
     <article className="teacher-item">
       <header>
-        <img src="https://avatars1.githubusercontent.com/u/16636590?s=460&u=9987bcee0f4496aa45c7fcb801f02748bd03fe0a&v=4" alt="Foto professor"/>  
+        <img src={teacher.avatar} alt="Foto professor"/>  
         <div>
-          <strong>Camila Nunes</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.
-        <br/><br/>
-        utilizado desde o século XVI, quando um impressor desconhecido pegou uma.
-      </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           Preço/Hora
-          <strong>R$75,00</strong>
+          <strong>R${teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="whatsapp"/>
           Entrar em Contato
-        </button>
+        </a>  
       </footer>
     </article>      
   )
